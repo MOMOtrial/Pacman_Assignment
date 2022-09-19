5ab0689db938d92397f3544e38af7802e2343b51
MichaelMOMO <109764421+MichaelMOMO@users.noreply.github.com>
2022-09-19 11:21:12 -0400

    Create main.c

---
+++ main.c
@@ -0,0 +1,18 @@
+//
+// Created by Michael Moser on 2022-09-17.
+//
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    // Declare variables
+    float totalMass;
+    float boxSpringMass;
+    float throneMass;
+    float armor;
+
+    // Get user input
+    printf("Mass of the box spring: ");
+    scanf("%f", boxSpringMass);
+    printf("\n");
+}
