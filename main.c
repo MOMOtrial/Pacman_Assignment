36c1f39af6f7f85f541c30d61213f0b6c9868fc6
MichaelMOMO <109764421+MichaelMOMO@users.noreply.github.com>
2022-09-19 18:13:51 -0400

    Update main.c

--- main.c
+++ main.c
@@ -4,23 +4,45 @@
 
 #include <stdio.h>
 #include <math.h>
-#define N 4.44822
+#define N 9.81
+#define e 2.71828
 
 int main() {
     // Declare variables
-    float totalMass;
+    float mass;
     float boxSpringMass;
     float throneMass;
-    float armorMass;
-    float massInN;
+    float massN;
+    float time;
+    int students;
+    int min;
+    int sec;
+
 
     // Get user input
-    printf("Please enter the mass (in pounds) of the box spring, throne, and armor respectively \n");
-    scanf("%f %f %f", &boxSpringMass, &throneMass, &armorMass);
+    printf("Please enter the mass of the box spring in kg: ");
+    scanf("%f", &boxSpringMass);
 
-    totalMass = boxSpringMass + throneMass + armorMass;
-    printf("The total mass is %0.2f pounds", totalMass);
+    printf("Please enter the mass of the throne in kg: ");
+    scanf("%f", &throneMass);
+
+    // Calculate total mass in pounds
+    mass = boxSpringMass + throneMass;
     printf("\n");
-    massInN = totalMass * N;
-    printf("%0.2f N", massInN);
+
+    // Calculate mass in Newtons
+    massN = mass * N;
+
+    // Calculate number of students needed to lift the ThunderMug
+    // One extra student added to assure safety
+    students = massN/300 + 1;
+    printf("The ThunderMugz will need a minimum of %d students to carry it.\n", students);
+
+    //Calculate time
+    //𝑡𝑖𝑚𝑒 = 𝑚𝑎𝑠𝑠 ∗ (4 − 𝑒^0.1𝑠𝑡𝑢𝑑𝑒𝑛𝑡𝑠)
+    time = mass * (4 - pow(e, 0.1*students));
+    int roundedTime = round(time);
+    min = time / 60;
+    sec = roundedTime % 60;
+    printf("It will take %d minutes and %d seconds to complete the ThunderMugz challenge.", min, sec);
 }
