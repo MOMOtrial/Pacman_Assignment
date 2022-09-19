85f9e4a09fad15dca050775a507aed8f782f7afb
MichaelMOMO <109764421+MichaelMOMO@users.noreply.github.com>
2022-09-19 13:15:48 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,18 +1,26 @@
 //
 // Created by Michael Moser on 2022-09-17.
 //
+
 #include <stdio.h>
 #include <math.h>
+#define N 4.44822
 
 int main() {
     // Declare variables
     float totalMass;
     float boxSpringMass;
     float throneMass;
-    float armor;
+    float armorMass;
+    float massInN;
 
     // Get user input
-    printf("Mass of the box spring: ");
-    scanf("%f", boxSpringMass);
+    printf("Please enter the mass (in pounds) of the box spring, throne, and armor respectively \n");
+    scanf("%f %f %f", &boxSpringMass, &throneMass, &armorMass);
+
+    totalMass = boxSpringMass + throneMass + armorMass;
+    printf("The total mass is %0.2f pounds", totalMass);
     printf("\n");
+    massInN = totalMass * N;
+    printf("%0.2f N", massInN);
 }
