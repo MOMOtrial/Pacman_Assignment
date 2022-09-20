ed7ce457fa7b9ab3c318cecba4df20e8c5c6afb1
MichaelMOMO <109764421+MichaelMOMO@users.noreply.github.com>
2022-09-20 12:25:12 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
 //
 // Created by Michael Moser on 2022-09-17.
+// I attest to the originality of my work
 //
 
 #include <stdio.h>
 #include <math.h>
 #define N 9.81
-#define e 2.71828
 
 int main() {
     // Declare variables
@@ -20,10 +20,10 @@ int main() {
 
 
     // Get user input
-    printf("Please enter the mass of the box spring in kg: ");
+    printf("Enter the mass of the box spring in kg:");
     scanf("%f", &boxSpringMass);
 
-    printf("Please enter the mass of the throne in kg: ");
+    printf("Enter the mass of the throne in kg:");
     scanf("%f", &throneMass);
 
     // Calculate total mass in pounds
@@ -35,13 +35,13 @@ int main() {
 
     // Calculate number of students needed to lift the ThunderMug
     // One extra student added to assure safety
-    students = massN/300 + 1;
+    students = ceil(massN/300);
     printf("The ThunderMugz will need a minimum of %d students to carry it.\n", students);
 
     //Calculate time
     //𝑡𝑖𝑚𝑒 = 𝑚𝑎𝑠𝑠 ∗ (4 − 𝑒^0.1𝑠𝑡𝑢𝑑𝑒𝑛𝑡𝑠)
-    time = mass * (4 - pow(e, 0.1*students));
-    int roundedTime = round(time);
+    time = mass * (4 - pow(M_E, 0.1*students));
+    int roundedTime = time;
     min = time / 60;
     sec = roundedTime % 60;
     printf("It will take %d minutes and %d seconds to complete the ThunderMugz challenge.", min, sec);
